67b7847a52af7c628906b40b38ea8a88a2ef44b6
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-12-21 17:20:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,46 +12,26 @@ client.user.setStatus("Do Not Distrurb")
  
 });
 
-client.on('message', message => {
-
+                           client.on('message', message => {
                                     var prefix = "$";
-
                             if (message.author.bot) return;
-
                             if (!message.content.startsWith(prefix)) return;
-
                         
-
                             let command = message.content.split(" ")[0];
-
                             command = command.slice(prefix.length);
-
                         
-
                             let args = message.content.split(" ").slice(1);
-
                         
-
                             if (command == "embed") {
-
                                 if (!message.channel.guild) return message.reply('** This command only for servers **');
-
                                 let say = new Discord.RichEmbed()
-
                                     .addField('Emebad:', `${message.author.username}#${message.author.discriminator}`)
-
                                     .setDescription(args.join("  "))
-
                                     .setColor(0x23b2d6)
-
                                 message.channel.sendEmbed(say);
-
                                 message.delete();
-
                             }
-
                         });
-
 
 
 
